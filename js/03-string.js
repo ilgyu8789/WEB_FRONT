76b89e6fad7aff25791b930c58917d6b997a0186
박일규 <t5nn123@naver.com>
2021-08-26 09:49:33 +0900

    string

--- js/03-string.js
+++ js/03-string.js
@@ -1,5 +1,5 @@
 //  JavaScript 문자열
-const s1 = "Modern JavaScript and Java";    //  Literal
+const s1 = "Modern JavaScript and Java"; //  Literal
 const s2 = String("Modern JavaScript"); //  String 객체 이용
 console.log(typeof s1, typeof s2);
 
@@ -8,35 +8,35 @@ console.log(typeof s1, typeof s2);
 console.log(s1, ", length=", s1.length);
 
 //  문자열 추출 메서드
-console.log(s1.charAt(7));  //  인덱스 7의 문자 확인
+console.log(s1.charAt(7)); //  인덱스 7의 문자 확인
 console.log(s1[7]); //  배열처럼 사용할 수 있으나 불변 자료형으로 내용 수정은 x
 //  구분 문자열 메서드
-console.log(s1.substr(7));  // 범위: 7번 인덱스 ~ 끝
-console.log(s1.substr(7, 10));  //  범위: 7번 인덱스 ~ 10글자
-console.log(s1.substring(7));   //  범위: 7번 인덱스 ~ 끝
-console.log(s1.substring(7, 17));   //  범위: 7번 인덱스 ~ 17번 인덱스 앞까지
+console.log(s1.substr(7)); // 범위: 7번 인덱스 ~ 끝
+console.log(s1.substr(7, 10)); //  범위: 7번 인덱스 ~ 10글자
+console.log(s1.substring(7)); //  범위: 7번 인덱스 ~ 끝
+console.log(s1.substring(7, 17)); //  범위: 7번 인덱스 ~ 17번 인덱스 앞까지
 
-console.log("------ Search ")
+console.log("------ Search ");
 console.log(s1, ", length=", s1.length);
 //  중요: 문자열 검색 메서드
 //  s1에서 Java를 검색
 
-let position = s1.indexOf("Java");  //  문자열 처음부터 Java를 검색 -> 대소문자 구분
+let position = s1.indexOf("Java"); //  문자열 처음부터 Java를 검색 -> 대소문자 구분
 console.log("1st Java:", position);
-position += "JavaScript".length;    //  검색 시작 위치 변경
-position = s1.indexOf("Java", position);    //  검색 범위 제한
+position += "JavaScript".length; //  검색 시작 위치 변경
+position = s1.indexOf("Java", position); //  검색 범위 제한
 console.log("2nd Java:", position);
-position += "Java".length;  //  검색 시작 위치 변경
-position = s1.indexOf("Java", position);    //  검색어 없음
-console.log("No keyword:", position);   //  찾는 검색어 없을 때는 -1 출력
+position += "Java".length; //  검색 시작 위치 변경
+position = s1.indexOf("Java", position); //  검색어 없음
+console.log("No keyword:", position); //  찾는 검색어 없을 때는 -1 출력
 
 //  문자열 치환: replace
-console.log(s1.replace("JavaScript", "JS"));   //   문자열 내의 JavaScript -> Java
+console.log(s1.replace("JavaScript", "JS")); //   문자열 내의 JavaScript -> Java
 //  String은 불변 자료형: 내부 데이터는 변경되지 않음
 console.log("원본:", s1);
 
 //  좌우 화이트 스페이스 제거: trim
-console.log("             Hello            ".trim());   //  좌우의 화이트 스페이스를 제거
+console.log("             Hello            ".trim()); //  좌우의 화이트 스페이스를 제거
 
 //  ----------- String 보충
 //  ", ' : 어떤 것을 사용해도 무방
@@ -48,11 +48,11 @@ console.log("             Hello            ".trim());   //  좌우의 화이트 
 //      \` : 백틱 (ES6)
 //      \$ : 달러기호 (ES6)
 
-let message = "He said, \"Hello\"";
+let message = 'He said, "Hello"';
 console.log(message);
 
 let temp = 24;
-//  현재 온도는 temp 도입니다. 
+//  현재 온도는 temp 도입니다.
 message = "현재 온도는 " + temp + "도 입니다.";
 console.log(message);
 
